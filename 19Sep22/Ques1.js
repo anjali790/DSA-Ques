ff315e5330cda03587dcde8c6e6c90e2b5a56cf9
anjali790 <anjaliruby790@gmail.com>
2022-09-20 14:16:14 +0530

    practice

--- 19Sep22/Ques1.js
+++ 19Sep22/Ques1.js
@@ -1,4 +1,4 @@
-// let nums = [1,3,5,6], target = 7;
+//Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 function searchInsert(nums, target){
     for(let i=0; i<nums.length; i++){
         if(nums.includes(target)){
