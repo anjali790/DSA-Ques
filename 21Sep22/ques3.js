76ce7422600f559ca60501be15ea8b978d4b4d35
anjali790 <anjaliruby790@gmail.com>
2022-09-25 17:15:16 +0530

    practice

--- 21Sep22/ques3.js
+++ 21Sep22/ques3.js
@@ -1,23 +1,18 @@
-//Given an array of integers arr, and three integers a, b and c. You need to find the number of good triplets.
-//A triplet (arr[i], arr[j], arr[k]) is good if the following conditions are true:
-//0 <= i < j < k < arr.length
-//|arr[i] - arr[j]| <= a
-//|arr[j] - arr[k]| <= b
-//|arr[i] - arr[k]| <= c
-//Where |x| denotes the absolute value of x.
-//Return the number of good triplets.
-
-function countGoodTriplets(arr, a, b, c) {
-    let count = 0;
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            for (let k = j + 1; k < arr.length; k++) {
-                if (Math.abs(arr[i] - arr[j] <= a) && Math.abs(arr[j] - arr[k] <= b) && Math.abs(arr[k] - arr[i] <= c)) {
-                    count++;
-                }
+//Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+//Return the answer in an array.
+// let nums = [6,5,4,8];
+let res=[];
+function smallerNumbersThanCurrent(nums){
+    for(let i=0; i<nums.length; i++){
+        let count=0;
+        for(let j=0; j<nums.length; j++){
+            if(j != i && nums[j] < nums[i]){
+                count++;
+                // res.push(count);
+                res[i]=count;
             }
         }
     }
-    return count;
+    return res;
 }
-console.log(countGoodTriplets([3, 0, 1, 1, 9, 7], 7, 2, 3))
+console.log(smallerNumbersThanCurrent([8,1,2,2,3]));
