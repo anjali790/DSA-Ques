169e1b11db408ddf5ebdf004ef5c57c14bcf82fe
anjali790 <anjaliruby790@gmail.com>
2022-09-30 17:19:10 +0530

    sorting

---
+++ 30Sep22/sorting.js
@@ -0,0 +1,15 @@
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length-1; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        } 
+        if (min !== i) {
+            [arr[i], arr[min]] = [arr[min], arr[i]];
+        }
+    }
+    return arr;
+}
+console.log(selectionSort([23,45,12,76,34,35]));
