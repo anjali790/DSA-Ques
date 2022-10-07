d968c535d9ec9b3527d872d737b08fee7975228b
anjali790 <anjaliruby790@gmail.com>
2022-10-07 20:38:49 +0530

    Bubble sort

---
+++ BubbleSort/app1.js
@@ -0,0 +1,14 @@
+function bubbleSort(array) {
+    
+    for(let i = 0; i < array.length-1; i++) {
+        for(let j = 0; j < array.length; j++) {
+            if(array[j] > array[j+1]){
+                let temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+    return array;
+}
+console.log(bubbleSort([11,45,2,78,56,34,89,0]))
---
+++ BubbleSort/app2.js
@@ -0,0 +1,18 @@
+function bubbleSort(array) {
+    let sorted = false;
+        
+    while (!sorted) {
+        sorted = true;
+        for(let i = 0; i < array.length-1; i++){
+            if(array[i] > array[i+1]){
+                [array[i], array[i+1]] = [array[i+1], array[i]];
+                // let temp = array[i];
+                // array[i] = array[i+1];
+                // array[i+1] = temp;
+                sorted = false;
+            }
+        }
+    }
+    return array;
+}
+console.log(bubbleSort([23,56,12,87,54,27]));
