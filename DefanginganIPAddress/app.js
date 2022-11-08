8e293fa40117e6463215ec7319e63d73e5a174c7
anjali790 <anjaliruby790@gmail.com>
2022-11-08 22:28:30 +0530

    Defanging an IP Address

--- DefanginganIPAddress/app.js
+++ DefanginganIPAddress/app.js
@@ -1,3 +1,5 @@
+//Given a valid (IPv4) IP address, return a defanged version of that IP address. A defanged IP address replaces every period "." with "[.]".
+
 var defangIPaddr = function (address) {
     return address.replaceAll(".", "[.]");
 };
